8d6b68e159a6adb770cccc9fb698107cbf19a851
firecracker37 <firecracker37@gmail.com>
2020-10-12 23:14:12 -0500

    Nightly Unstable Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 //Feature 1
-
 import React from 'react';
+import data from './data.json'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="grid-container">
-        <header>
-          <a href="/">React Shopping Cart</a>
-        </header>
-        <main>
-          Product List
-        </main>
-        <footer>
-          All Rights Reserved
-        </footer>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">Products</div>
+              <div className="sidebar">Cart Items</div>
+            </div>
+          </main>
+          <footer>
+            All Rights Reserved
+          </footer>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
